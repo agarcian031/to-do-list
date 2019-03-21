7d505ba6cfefc86015ec7f67f655c8e7cff1dabe
agarcian031 <algarcian@outlook.com>
2019-03-21 15:24:04 -0400

    Change class='date' to id and fix app.js error

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // SELECT ELEMENTS 
 const clear = document.querySelector(".clear"); 
-const dateElement = document.getElementById("date"); 
+const dateElement= document.getElementById("date"); 
 const list = document.getElementById("list"); 
 const input = document.getElementById("input"); 
 
@@ -8,3 +8,8 @@ const input = document.getElementById("input");
 const CHECK = "fa-check-circle"; 
 const UNCHECK = "fa-circle-thin"; 
 const LINE_THROUGH = "lineThrough"; 
+
+//SHOW TODAYS DATE 
+const options = {weekday:"long", month:"short", day:"numeric"}; 
+const today = new Date(); 
+dateElement.innerHTML  = today.toLocaleDateString("en-US", options); 
